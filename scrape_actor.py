a5a879aa97c526175da7d97821730eb0eb0149d7
John Walters <johnmwalters@gmail.com>
2015-10-01 19:00:47 -0400

    updated the same as ipython notebook, complete url in columns

--- scrape_actor.py
+++ scrape_actor.py
@@ -65,9 +65,25 @@ def movie_table(all_movie_html_block):
         movie_table.append(movie_items)
     return movie_table
 
-movie_table1 = movie_table(all_movie_html_block_1)
-movie_table2 = movie_table(all_movie_html_block_2)
-col = ['date', 'date_url', 'movie', 'movie_url', 'studio', 'studio_rul', 'gross', 'gross_url', 'theaters_g', 'theaters_g_url', 'opening', 'opening_url', 'theaters_o', 'theaters_o_url', 'rank', 'rank_url']
+movie_table1 = movie_table(all_movie_html_block)
+movie_table2 = movie_table(all_movie_html_block_adj)
+col = ['date', 'date_url', 'movie', 'movie_url', 'studio', 'studio_url', 'gross', 'gross_url', 'theaters_g', 'theaters_g_url', 'opening', 'opening_url', 'theaters_o', 'theaters_o_url', 'rank', 'rank_url']
 nc_movies_1 = pd.DataFrame(movie_table1, columns = col)
+nc_movies_1['date_url'] = 'http://www.boxofficemojo.com/'+nc_movies_1['date_url']
+nc_movies_1['movie_url'] = 'http://www.boxofficemojo.com/'+nc_movies_1['movie_url']
+nc_movies_1['studio_url'] = 'http://www.boxofficemojo.com/'+nc_movies_1['studio_url']
+nc_movies_1['gross_url'] = 'http://www.boxofficemojo.com/'+nc_movies_1['gross_url']
+nc_movies_1['theaters_g_url'] = 'http://www.boxofficemojo.com/'+nc_movies_1['theaters_g_url']
+nc_movies_1['opening_url'] = 'http://www.boxofficemojo.com/'+nc_movies_1['opening_url']
+nc_movies_1['theaters_o_url'] = 'http://www.boxofficemojo.com/'+nc_movies_1['theaters_o_url']
+nc_movies_1['rank_url'] = 'http://www.boxofficemojo.com/'+nc_movies_1['rank_url']
 col = ['rank', 'rank_url', 'movie', 'movie_url', 'studio', 'studio_url','adjusted_gross', 'adjusted_gross_url', 'adjusted_opening', 'adjusted_opening_url', 'date', 'date_url']
 nc_movies_2 = pd.DataFrame(movie_table2, columns = col)
+nc_movies_2['date_url'] = 'http://www.boxofficemojo.com/'+nc_movies_1['date_url']
+nc_movies_2['movie_url'] = 'http://www.boxofficemojo.com/'+nc_movies_1['movie_url']
+nc_movies_2['studio_url'] = 'http://www.boxofficemojo.com/'+nc_movies_1['studio_url']
+nc_movies_2['gross_url'] = 'http://www.boxofficemojo.com/'+nc_movies_1['gross_url']
+nc_movies_2['theaters_g_url'] = 'http://www.boxofficemojo.com/'+nc_movies_1['theaters_g_url']
+nc_movies_2['opening_url'] = 'http://www.boxofficemojo.com/'+nc_movies_1['opening_url']
+nc_movies_2['theaters_o_url'] = 'http://www.boxofficemojo.com/'+nc_movies_1['theaters_o_url']
+nc_movies_2['rank_url'] = 'http://www.boxofficemojo.com/'+nc_movies_1['rank_url']
